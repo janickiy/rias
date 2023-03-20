ea79e6336f5331aad19598ee54ba27e1e12c0b1b
Яницкий Александр Анатольевич <a.yanitskiy@els24.com>
2023-03-20 17:30:25 +0300

    bug fix

--- resources/views/cp/catalog/create_edit.blade.php
+++ resources/views/cp/catalog/create_edit.blade.php
@@ -42,7 +42,7 @@
                     <!-- widget content -->
                     <div class="widget-body">
 
-                        {!! Form::open(['url' => isset($row) ? URL::route('cp.catalog.update') : URL::route('cp.catalog.store'), 'method' => isset($row) ? 'put' : 'post', 'class' => "smart-form"]) !!}
+                        {!! Form::open(['url' => isset($row) ? URL::route('cp.catalog.update') : URL::route('cp.catalog.store'), 'files' => true, 'method' => isset($row) ? 'put' : 'post', 'class' => "smart-form"]) !!}
 
                         {!! isset($row) ? Form::hidden('id', $row->id) : '' !!}
 
